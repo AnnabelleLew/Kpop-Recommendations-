8752f6d72c9e751057922ff09facb5f1ced11108
Girls Who Code <GWC@cs81-ms-crt-13.local>
2019-07-09 13:55:11 -0400

    Initial Commit

---
+++ Kpop Recommendations!Tests/Kpop_Recommendations_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Kpop_Recommendations_Tests.swift
+//  Kpop Recommendations!Tests
+//
+//  Created by Girls Who Code on 7/9/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import XCTest
+@testable import Kpop_Recommendations_
+
+class Kpop_Recommendations_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
